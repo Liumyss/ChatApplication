284b814db689acfcdd1baf483eb095063e988854
Liumyss <92648495+Liumyss@users.noreply.github.com>
2023-02-06 17:06:19 +0100

    final version (admin support added)

--- client.py
+++ client.py
@@ -2,16 +2,23 @@ import socket
 import threading
 
 # CONSTANTS
-HEADER = 64
-SERVER = "127.0.0.1"
-PORT = 5050
+HEADER = 1024
+SERVER = "localhost"
+PORT = 8000
 HOST = (SERVER, PORT)
 FORMAT = 'utf-8'
+
+stop_thread = False
 
 class ClientSide():
 
     def __init__(self):
-        self.nickname = input("Choose a nickname: ")
+
+        # CONNECT TO THE SERVER
+        self.username = input("Enter your nickname: ")
+
+        if self.username == "admin":
+            self.password = input("Enter your password: ")
 
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -23,10 +30,31 @@ class ClientSide():
 
     def receive(self):
         while True:
+
+            global stop_thread
+            if stop_thread:
+                break
+
             try:
-                message = self.client.recv(1024).decode(FORMAT)
-                if message == "NICK":
-                    self.client.send(self.nickname.encode(FORMAT))
+                message = self.client.recv(HEADER).decode(FORMAT)
+
+                if message == "NAME":
+                    self.client.send(self.username.encode(FORMAT))
+
+                    next_message = self.client.recv(HEADER).decode(FORMAT)
+
+                    # ADMIN AUTHENTIFICATION
+                    if next_message == "ADMIN":
+                        self.client.send(self.password.encode(FORMAT))
+                        if self.client.recv(HEADER).decode(FORMAT) == "DENY":
+                            print("[ERROR] Wrong password")
+                            stop_thread = True
+                    
+                    # TELL USER IF BANNED - CONNECTION REFUSED IF BANNED
+                    elif next_message == "BAN":
+                        print("[ERROR] Connection refused. You are banned from the server.")
+                        self.client.close()
+                        stop_thread = True
                 else:
                     print(message)
             except:
@@ -36,8 +64,26 @@ class ClientSide():
 
     def write(self):
         while True:
-            message = f'{self.nickname}: {input("")}'
-            self.client.send(message.encode(FORMAT))
+
+            if stop_thread:
+                break
+            
+            # MODEL OF A MESSAGE SENT TO THE SERVER
+            message = f"{self.username}: {input('')}"
+
+            # COMMANDS OF THE SERVER
+            if message[len(self.username)+2:].startswith("/"):
+                if self.username == "admin":
+                    if message[len(self.username)+2:].startswith("/kick"):
+                        self.client.send(f"KICK {message[len(self.username)+2+6:]}".encode(FORMAT))
+                    if message[len(self.username)+2:].startswith("/ban"):
+                        self.client.send(f"BAN {message[len(self.username)+2+5:]}".encode(FORMAT))
+                    if message.startswith("/clearban"):
+                        self.client.send("CLEAR").encode(FORMAT)
+                else:
+                    print("[ERROR] Commands can only be executed by an admin")
+            else:
+                self.client.send(message.encode(FORMAT))
 
 
 Client = ClientSide()
@@ -47,6 +93,3 @@ receive_thread.start()
 
 write_thread = threading.Thread(target=Client.write)
 write_thread.start()
-
-# ADD AUTHENTIFICATION LOGINS
-# ADD DATABASE TO KEEP THE LOGS OF MESSAGES AND INFOS
--- server.py
+++ server.py
@@ -1,64 +1,136 @@
-import socket 
+import socket
 import threading
 
 # CONSTANTS
-HEADER = 64
-SERVER = socket.gethostbyname(socket.gethostname())
-PORT = 5050
+HEADER = 1024
+SERVER = "localhost"
+PORT = 8000
 HOST = (SERVER, PORT)
 FORMAT = 'utf-8'
 
 clients = []
-nicknames = []
+usernames = []
+
 
 class ServerSide:
 
     def __init__(self):
-        # SERVER PREPARATION AND BINDING TO THE RIGHT HOST
+    
+        # INITIALIZE THE SERVER 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind(HOST)
 
+    # SEND MESSAGE TO ALL THE ACTIVE USERS OF THE CHAT SERVER
     def broadcast(self, message):
         for client in clients:
             client.send(message)
 
-    # HANDLE THE CONNECTIONS OF THE CLIENTS
+    # HANDLE THE CONNECTIONS OF EACH CLIENT
     def handle(self, client):
 
         while True:
             try:
-                message = client.recv(1024)
-                self.broadcast(message)
+                msg = message = client.recv(HEADER)
+
+                # REQUEST TO KICK A CLIENT
+                if msg.decode(FORMAT).startswith("KICK"):
+                    if usernames[clients.index(client)] == "admin":
+                        user_to_kick = msg.decode(FORMAT)[5:]
+                        self.kick(user_to_kick, "k")
+                    else:
+                        client.send("[WARNING] Command was refused. You are not an admin!".encode(FORMAT))
+
+                # REQUEST TO BAN A CLIENT
+                elif msg.decode(FORMAT).startswith("BAN"):
+                    if usernames[clients.index(client)] == "admin":
+                        user_to_ban = msg.decode(FORMAT)[4:]
+                        self.kick(user_to_ban, "b")
+
+                        with open("bans.txt", "a") as f:
+                            f.write(f"{user_to_ban}\n")
+
+                        print(f"{user_to_ban} was banned")
+                    else:
+                        client.send("[WARNING] Command was refused. You are not an admin!".encode(FORMAT))
+
+                # REQUEST TO CLEAR ALL THE BANS
+                elif msg.decode(FORMAT).startswith("CLEAR"):
+                    if usernames[clients.index(client)] == "admin":
+                        f = open("bans.txt","w")
+                        f.close()
+                    else:
+                        client.send("[WARNING] Command was refused. You are not an admin!".encode(FORMAT))
+                    
+                else:
+                    self.broadcast(message)
             except:
                 index = clients.index(client)
                 clients.remove(client)
                 client.close()
-                nickname = nicknames[index]
-                self.broadcast(f"{nickname} left he chat".encode(FORMAT))
-                nicknames.remove(nickname)
+                username = usernames[index]
+                self.broadcast(f"{username} left he chat".encode(FORMAT))
+                usernames.remove(username)
                 break
 
     def start(self):
         self.server.listen()
-        print(f"[LISTENING] Server is listening on {SERVER}")
+        print(f"[LISTENING] Server is listening on {SERVER}:{PORT}")
         while True:
             client, addr = self.server.accept()
             print(f"[CONNECTED] {addr}")
+            
+            # CREATE AN USERNAME FOR THE CLIENT
+            client.send("NAME".encode(FORMAT))
+            username = client.recv(HEADER).decode(FORMAT)
 
-            client.send("NICK".encode(FORMAT))
-            nickname = client.recv(1024).decode(FORMAT)
-            nicknames.append(nickname)
+            # READ THE BANS TEXT FILE TO CHECK IF USERNAME IS BANNED
+            check_ban = False
+            with open("bans.txt", "r") as f:
+                for line in f:
+                    if username in line:
+                        check_ban = True
+
+            if check_ban:
+                client.send("BAN".encode(FORMAT))
+                client.close()
+                continue
+            
+            # REQUEST PASSWORD IF ADMIN
+            if username == "admin":
+                client.send("ADMIN".encode(FORMAT))
+                password = client.recv(HEADER).decode(FORMAT)
+
+                if password != "admin":
+                    client.send("DENY".encode(FORMAT))
+                    client.close()
+                    continue
+
+            usernames.append(username)
             clients.append(client)
 
-            print(f"Nickname of the client is {nickname}")
-            self.broadcast(f"{nickname} joined the chat".encode(FORMAT))
+            # CLIENT JOIN THE SERVER
+            print(f"Nickname of the client is {username}")
+            self.broadcast(f"{username} joined the chat".encode(FORMAT))
             client.send("Connected to the server".encode(FORMAT))
 
             thread = threading.Thread(target=self.handle, args=(client,))
-            thread.start()
-            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-            print(f"Active usersnames: {nicknames}")
+            thread.start()   
 
+    # METHOD TO KICK USER FROM CHAT SERVER
+    def kick(self, user, choice):
+        if user in usernames:
+            user_index = usernames.index(user)
+            client_to_kick = clients[user_index]
+            clients.remove(client_to_kick)
+            client_to_kick.send("You were kicked from the server".encode(FORMAT))
+            client_to_kick.close()
+            usernames.remove(user)
+
+            # DISPLAY IF USER WAS KICKED OR BANNED
+            if choice == "k":
+                self.broadcast(f"{user} was kicked".encode(FORMAT))
+            elif choice == "b":
+                self.broadcast(f"{user} was banned".encode(FORMAT))
 
 print("[STARTING] Server is running..")
 Server = ServerSide()
